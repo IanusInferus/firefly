eac818926193ffeca8dcf016a8477d13bce6d1e0
Ianus Inferus <r_ex@163.com>
2010-09-15 03:54:51 +0000

    Modify some useless commands batch.

--- Src/Publish.cmd
+++ Src/Publish.cmd
@@ -8,10 +8,6 @@
 ::@set PackName=PackageName
 
 @call Clear.cmd
-@del ..\Bin\System.Core.dll /F /Q
-@del ..\Bin\System.Speech.dll /F /Q
-@del ..\Bin\System.Xml.Linq.dll /F /Q
-@rd ..\Bin\zh-CHS /S /Q
 @cd ..
 @del %PackName%.rar
 @rar a -av- -m5 -md4096 -tsm -tsc -s -k -t %PackName%.rar -x*\.svn -xSrc\*.user Src Bin Manual Examples
