047c20cc707fbc1f0de39fb7fed869f796dfc79c
Ianus Inferus <r_ex@163.com>
2010-11-17 08:41:20 +0000

    Examples: 完成SerializerExample，C#版。

---
+++ Examples/Mapping/C#/SerializerExample/Src/Program.cs
@@ -0,0 +1,47 @@
+﻿//==========================================================================
+//
+//  File:        Program.cs
+//  Location:    Firefly.Examples <Visual C#>
+//  Description: 二进制序列化器示例
+//  Version:     2010.11.17.
+//  Author:      F.R.C.
+//  Copyright(C) public Domain
+//
+//==========================================================================
+
+using System;
+using System.IO;
+using Firefly;
+
+namespace PackageManager
+{
+    public static class Program
+    {
+        [STAThread]
+        public static int Main()
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                return MainWindow();
+            }
+            else
+            {
+                try
+                {
+                    return MainWindow();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ExceptionInfo.GetExceptionInfo(ex));
+                    return -1;
+                }
+            }
+        }
+
+        public static int MainWindow()
+        {
+            Example.Execute();
+            return 0;
+        }
+    }
+}
