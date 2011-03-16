8126358a0d3508a8e9b189ac670815cdcab4c15f
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2011-03-16 04:24:46 +0000

    Firefly.Core.Texting: 修正无法通过XML头自动识别编码的问题。

--- Src/FireflyCore/Texting/XmlFile.vb
+++ Src/FireflyCore/Texting/XmlFile.vb
@@ -15,39 +15,45 @@ Imports System.Xml
 Imports System.Xml.Linq
 Imports System.Text
 Imports Firefly
-Imports Firefly.Texting
 Imports Firefly.Mapping
 Imports Firefly.Mapping.XmlText
+Imports Firefly.Streaming
+Imports Firefly.Texting
 
 Namespace Texting
     Public NotInheritable Class XmlFile
         Private Sub New()
         End Sub
 
+        Private Shared DefaultReaderSetting As New XmlReaderSettings With {.CheckCharacters = False}
         Public Shared Function ReadFile(ByVal Path As String) As XElement
-            Return ReadFile(Path, TextEncoding.Default)
-        End Function
-        Public Shared Function ReadFile(ByVal Path As String, ByVal Encoding As Encoding) As XElement
-            Using s As New FileStream(Path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
-                Using sr As New StreamReader(s, Encoding, True)
-                    Return ReadFile(sr)
-                End Using
-            End Using
+            Return ReadFile(Path, DefaultReaderSetting)
         End Function
         Public Shared Function ReadFile(ByVal Path As String, Setting As XmlReaderSettings) As XElement
-            Return ReadFile(Path, TextEncoding.Default, Setting)
+            Using s = Streams.OpenReadable(Path)
+                Return ReadFile(s, Setting)
+            End Using
+        End Function
+        Public Shared Function ReadFile(ByVal s As IReadableStream) As XElement
+            Return ReadFile(s, DefaultReaderSetting)
+        End Function
+        Public Shared Function ReadFile(ByVal s As IReadableStream, Setting As XmlReaderSettings) As XElement
+            Using r = XmlReader.Create(s.ToUnsafeStream, Setting)
+                Return XElement.Load(r, LoadOptions.PreserveWhitespace Or LoadOptions.SetLineInfo)
+            End Using
+        End Function
+        Public Shared Function ReadFile(ByVal Path As String, ByVal Encoding As Encoding) As XElement
+            Return ReadFile(Path, Encoding, DefaultReaderSetting)
         End Function
         Public Shared Function ReadFile(ByVal Path As String, ByVal Encoding As Encoding, Setting As XmlReaderSettings) As XElement
-            Using s As New FileStream(Path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
-                Using sr As New StreamReader(s, Encoding, True)
+            Using s = Streams.OpenReadable(Path)
+                Using sr = Txt.CreateTextReader(s.AsNewReading, Encoding)
                     Return ReadFile(sr, Setting)
                 End Using
             End Using
         End Function
         Public Shared Function ReadFile(ByVal Reader As StreamReader) As XElement
-            Using r = XmlReader.Create(Reader, New XmlReaderSettings With {.CheckCharacters = False})
-                Return XElement.Load(r, LoadOptions.PreserveWhitespace Or LoadOptions.SetLineInfo)
-            End Using
+            Return ReadFile(Reader, DefaultReaderSetting)
         End Function
         Public Shared Function ReadFile(ByVal Reader As StreamReader, Setting As XmlReaderSettings) As XElement
             Using r = XmlReader.Create(Reader, Setting)
