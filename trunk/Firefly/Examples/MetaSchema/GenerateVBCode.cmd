0613fa0ae075d2de81ee47d0b03a56c4ec5ae2d1
r_ex <r_ex@8bc0d464-4f16-4369-b535-01149e33d79d>
2011-03-16 06:28:58 +0000

    Examples\MetaSchema: 增加元类型结构代码生成示例。

---
+++ trunk/Firefly/Examples/MetaSchema/GenerateVBCode.cmd
@@ -0,0 +1,3 @@
+..\..\Bin\MetaSchemaManipulator.exe /t2vb:BinarySchemaBasic.tree,BinarySchemaBasic.vb,BinarySchema
+..\..\Bin\MetaSchemaManipulator.exe /t2vb:BinarySchemaExtensive.tree,BinarySchemaExtensive.vb,BinarySchema
+pause
