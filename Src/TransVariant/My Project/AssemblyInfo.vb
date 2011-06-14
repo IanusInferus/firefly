63d42c4f619ae26a37898a58ca4ac4f27cad99e7
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2011-06-14 04:26:43 +0000

    Firefly.Core.Texting, Firefly.Core.Setting, Firefly.TransVariant: 修正一些写法问题。

--- Src/TransVariant/My Project/AssemblyInfo.vb
+++ Src/TransVariant/My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 ' 方法是按如下所示使用“*”:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2010.08.28.0")> 
+<Assembly: AssemblyVersion("2011.06.14.0")> 
