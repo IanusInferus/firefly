2a3be01abeaccc8559e90d6e6895b791710eae0b
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2011-07-31 07:32:44 +0000

    Examples\MetaSchema: 增加C#的代码生成例子。

---
+++ Examples/MetaSchema/GenerateCsCode.cmd
@@ -0,0 +1,3 @@
+..\..\Bin\MetaSchemaManipulator.exe /t2cs:BinarySchemaBasic.tree,BinarySchemaBasic.cs,BinarySchema
+..\..\Bin\MetaSchemaManipulator.exe /t2cs:BinarySchemaExtensive.tree,BinarySchemaExtensive.cs,BinarySchema
+pause
