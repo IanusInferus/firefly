31c86efe160190d2e4951ce0f5fc1a560b45d151
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2011-08-01 15:12:34 +0000

    Examples\MetaSchema: 增加F#的代码生成例子。

---
+++ Examples/MetaSchema/GenerateFsCode.cmd
@@ -0,0 +1,3 @@
+..\..\Bin\MetaSchemaManipulator.exe /t2fs:BinarySchemaBasic.tree,BinarySchemaBasic.fs,BinarySchema
+..\..\Bin\MetaSchemaManipulator.exe /t2fs:BinarySchemaExtensive.tree,BinarySchemaExtensive.fs,BinarySchema
+pause
