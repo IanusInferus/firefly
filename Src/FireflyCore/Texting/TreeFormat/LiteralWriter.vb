247d71e4ad53ffbef70c5a9322aa72a3d6e67164
Ianus Inferus <r_ex@163.com>
2011-08-29 18:37:07 +0000

    Firefly.Core.Texting.TreeFormat: 修正函数最后的空行没有被忽略的问题。 增加Tree文件格式原始输出功能。 Firefly.Core.Texting: 增加半角字符判断。

---
+++ Src/FireflyCore/Texting/TreeFormat/LiteralWriter.vb
@@ -0,0 +1,141 @@
+﻿'==========================================================================
+'
+'  File:        LiteralWriter.vb
+'  Location:    Firefly.Texting.TreeFormat <Visual Basic .Net>
+'  Description: 字面量输出类
+'  Version:     2011.08.30.
+'  Copyright(C) F.R.C.
+'
+'==========================================================================
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Diagnostics
+Imports System.Text.RegularExpressions
+Imports System.IO
+Imports Firefly.Mapping.MetaSchema
+Imports Firefly.TextEncoding
+Imports Firefly.Texting.TreeFormat.Semantics
+
+Namespace Texting.TreeFormat
+    Public Enum LiteralTag
+        SingleLine
+        MultiLine
+    End Enum
+    <TaggedUnion(), DebuggerDisplay("{ToString()}")>
+    Public Class Literal
+        <Tag()> Public Property _Tag As LiteralTag
+        Public Property SingleLine As String
+        Public Property MultiLine As String()
+
+        Public Shared Function CreateSingleLine(ByVal Value As String) As Literal
+            Return New Literal With {._Tag = LiteralTag.SingleLine, .SingleLine = Value}
+        End Function
+        Public Shared Function CreateMultiLine(ByVal Value As String()) As Literal
+            Return New Literal With {._Tag = LiteralTag.MultiLine, .MultiLine = Value}
+        End Function
+
+        Public ReadOnly Property OnSingleLine() As Boolean
+            Get
+                Return _Tag = LiteralTag.SingleLine
+            End Get
+        End Property
+        Public ReadOnly Property OnMultiLine() As Boolean
+            Get
+                Return _Tag = LiteralTag.MultiLine
+            End Get
+        End Property
+
+        Public Overrides Function ToString() As String
+            Return DebuggerDisplayer.ConvertToString(Me)
+        End Function
+    End Class
+
+    Public NotInheritable Class TreeFormatLiteralWriter
+        Private Sub New()
+        End Sub
+
+        Private Shared ReadOnly Empty As String = "$Empty"
+        Private Enum ParentheseType
+            Angle
+            Bracket
+            Brace
+        End Enum
+        Private Shared rCrLf As New Regex("\r\n", RegexOptions.ExplicitCapture)
+        Private Shared rWildCrOrLf As New Regex("\r(?!\n)|(?<!\r)\n", RegexOptions.ExplicitCapture)
+        Private Shared rForbiddenChars As New Regex("[()\f\t\v]", RegexOptions.ExplicitCapture)
+        Private Shared rForbiddenHeadChars As New Regex("^[!%&;=?\^`|~]", RegexOptions.ExplicitCapture)
+        Public Shared Function GetLiteral(ByVal Value As String, ByVal MustSingleLine As Boolean, ByVal MustMultiLine As Boolean) As Literal
+            If Value Is Nothing Then
+                If MustMultiLine Then
+                    Return Literal.CreateMultiLine(New String() {})
+                Else
+                    Return Literal.CreateSingleLine(Empty)
+                End If
+            End If
+            If Value = "" Then
+                If MustMultiLine Then
+                    Return Literal.CreateMultiLine(New String() {""})
+                Else
+                    Return Literal.CreateSingleLine("""""")
+                End If
+            End If
+            If MustMultiLine Then
+                Return Literal.CreateMultiLine(rCrLf.Split(Value.UnifyNewLineToCrLf()))
+            End If
+            Dim wm = rWildCrOrLf.Match(Value).Success
+            Dim cm = rCrLf.Match(Value).Success
+            If wm OrElse (cm AndAlso MustSingleLine) Then
+                Dim s = Value.Escape().Replace("""", "\""")
+                If s.StartsWith(" ") Then
+                    Return Literal.CreateSingleLine("""""\" & s & """""")
+                Else
+                    Return Literal.CreateSingleLine("""""" & s & """""")
+                End If
+            End If
+            If cm Then Return Literal.CreateMultiLine(rCrLf.Split(Value))
+
+            Dim CreateQuotationLiteral = Function() Literal.CreateSingleLine("""" & Value.Replace("""", """""") & """")
+
+            If rForbiddenHeadChars.Match(Value).Success OrElse rForbiddenChars.Match(Value).Success Then
+                Return CreateQuotationLiteral()
+            End If
+
+            Dim ParentheseStack As New Stack(Of ParentheseType)
+            For Each c In Value
+                Dim cs = New String(c, 1)
+                Select Case cs
+                    Case """", " "
+                        Return CreateQuotationLiteral()
+                    Case "<"
+                        ParentheseStack.Push(ParentheseType.Angle)
+                    Case "["
+                        ParentheseStack.Push(ParentheseType.Bracket)
+                    Case "{"
+                        ParentheseStack.Push(ParentheseType.Brace)
+                    Case ">"
+                        If ParentheseStack.Count = 0 OrElse ParentheseStack.Peek <> ParentheseType.Angle Then
+                            Return CreateQuotationLiteral()
+                        End If
+                        ParentheseStack.Pop()
+                    Case "]"
+                        If ParentheseStack.Count = 0 OrElse ParentheseStack.Peek <> ParentheseType.Bracket Then
+                            Return CreateQuotationLiteral()
+                        End If
+                        ParentheseStack.Pop()
+                    Case "}"
+                        If ParentheseStack.Count = 0 OrElse ParentheseStack.Peek <> ParentheseType.Brace Then
+                            Return CreateQuotationLiteral()
+                        End If
+                        ParentheseStack.Pop()
+                    Case Else
+
+                End Select
+            Next
+            If ParentheseStack.Count <> 0 Then Return CreateQuotationLiteral()
+
+            Return Literal.CreateSingleLine(Value)
+        End Function
+    End Class
+End Namespace
--- Src/FireflyCore/Texting/TreeFormat/Writer.vb
+++ Src/FireflyCore/Texting/TreeFormat/Writer.vb
@@ -3,7 +3,7 @@
 '  File:        Writer.vb
 '  Location:    Firefly.Texting.TreeFormat <Visual Basic .Net>
 '  Description: 文本输出类
-'  Version:     2011.07.31.
+'  Version:     2011.08.30.
 '  Copyright(C) F.R.C.
 '
 '==========================================================================
@@ -26,9 +26,9 @@ Namespace Texting.TreeFormat
         End Sub
 
         Private Shared ReadOnly Empty As String = "$Empty"
-        Private Shared ReadOnly List As String = "$List"
         Private Shared ReadOnly StringDirective As String = "$String"
         Private Shared ReadOnly EndDirective As String = "$End"
+        Private Shared ReadOnly List As String = "$List"
 
         Public Sub Write(ByVal Forest As Forest)
             For Each n In Forest.Nodes
@@ -134,104 +134,8 @@ Namespace Texting.TreeFormat
             sw.WriteLine(si & Value)
         End Sub
 
-
-        Private Enum LiteralTag
-            SingleLine
-            MultiLine
-        End Enum
-        <TaggedUnion(), DebuggerDisplay("{ToString()}")>
-        Private Class Literal
-            <Tag()> Public Property _Tag As LiteralTag
-            Public Property SingleLine As String
-            Public Property MultiLine As String()
-
-            Public Shared Function CreateSingleLine(ByVal Value As String) As Literal
-                Return New Literal With {._Tag = LiteralTag.SingleLine, .SingleLine = Value}
-            End Function
-            Public Shared Function CreateMultiLine(ByVal Value As String()) As Literal
-                Return New Literal With {._Tag = LiteralTag.MultiLine, .MultiLine = Value}
-            End Function
-
-            Public ReadOnly Property OnSingleLine() As Boolean
-                Get
-                    Return _Tag = LiteralTag.SingleLine
-                End Get
-            End Property
-            Public ReadOnly Property OnMultiLine() As Boolean
-                Get
-                    Return _Tag = LiteralTag.MultiLine
-                End Get
-            End Property
-
-            Public Overrides Function ToString() As String
-                Return DebuggerDisplayer.ConvertToString(Me)
-            End Function
-        End Class
-
-        Private Enum ParentheseType
-            Angle
-            Bracket
-            Brace
-        End Enum
-        Private Shared rCrLf As New Regex("\r\n", RegexOptions.ExplicitCapture)
-        Private Shared rWildCrOrLf As New Regex("\r(?!\n)|(?<!\r)\n", RegexOptions.ExplicitCapture)
-        Private Shared rForbiddenChars As New Regex("[()\f\t\v]", RegexOptions.ExplicitCapture)
-        Private Shared rForbiddenHeadChars As New Regex("^[!%&;=?\^`|~]", RegexOptions.ExplicitCapture)
-        Private Shared Function GetLiteral(ByVal Value As String, ByVal MustSingleLine As Boolean) As Literal
-            If Value Is Nothing Then Return Literal.CreateSingleLine(Empty)
-            If Value = "" Then Return Literal.CreateSingleLine("""""")
-            Dim wm = rWildCrOrLf.Match(Value).Success
-            Dim cm = rCrLf.Match(Value).Success
-            If wm OrElse (cm AndAlso MustSingleLine) Then
-                Dim s = Value.Escape().Replace("""", "\""")
-                If s.StartsWith(" ") Then
-                    Return Literal.CreateSingleLine("""""\" & s & """""")
-                Else
-                    Return Literal.CreateSingleLine("""""" & s & """""")
-                End If
-            End If
-            If cm Then Return Literal.CreateMultiLine(rCrLf.Split(Value))
-
-            Dim CreateQuotationLiteral = Function() Literal.CreateSingleLine("""" & Value.Replace("""", """""") & """")
-
-            If rForbiddenHeadChars.Match(Value).Success OrElse rForbiddenChars.Match(Value).Success Then
-                Return CreateQuotationLiteral()
-            End If
-
-            Dim ParentheseStack As New Stack(Of ParentheseType)
-            For Each c In Value
-                Dim cs = New String(c, 1)
-                Select Case cs
-                    Case """", " "
-                        Return CreateQuotationLiteral()
-                    Case "<"
-                        ParentheseStack.Push(ParentheseType.Angle)
-                    Case "["
-                        ParentheseStack.Push(ParentheseType.Bracket)
-                    Case "{"
-                        ParentheseStack.Push(ParentheseType.Brace)
-                    Case ">"
-                        If ParentheseStack.Count = 0 OrElse ParentheseStack.Peek <> ParentheseType.Angle Then
-                            Return CreateQuotationLiteral()
-                        End If
-                        ParentheseStack.Pop()
-                    Case "]"
-                        If ParentheseStack.Count = 0 OrElse ParentheseStack.Peek <> ParentheseType.Bracket Then
-                            Return CreateQuotationLiteral()
-                        End If
-                        ParentheseStack.Pop()
-                    Case "}"
-                        If ParentheseStack.Count = 0 OrElse ParentheseStack.Peek <> ParentheseType.Brace Then
-                            Return CreateQuotationLiteral()
-                        End If
-                        ParentheseStack.Pop()
-                    Case Else
-
-                End Select
-            Next
-            If ParentheseStack.Count <> 0 Then Return CreateQuotationLiteral()
-
-            Return Literal.CreateSingleLine(Value)
+        Private Shared Function GetLiteral(ByVal Value As String, ByVal MustSingleLine As Boolean, Optional MustMultiLine As Boolean = False) As Literal
+            Return TreeFormatLiteralWriter.GetLiteral(Value, MustSingleLine, MustMultiLine)
         End Function
     End Class
 End Namespace
