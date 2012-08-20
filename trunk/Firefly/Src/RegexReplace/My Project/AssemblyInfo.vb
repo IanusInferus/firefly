cbeb82441ec14af5d6380ac4721684d03595c583
r_ex <r_ex@8bc0d464-4f16-4369-b535-01149e33d79d>
2012-08-20 12:59:23 +0000

    Firefly.RegexReplace: 增加替换模式转义，使得替换模式中可以表达换行等字符

--- trunk/Firefly/Src/RegexReplace/My Project/AssemblyInfo.vb
+++ trunk/Firefly/Src/RegexReplace/My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 ' 方法是按如下所示使用“*”:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2012.07.12.0")> 
+<Assembly: AssemblyVersion("2012.08.20.0")> 
