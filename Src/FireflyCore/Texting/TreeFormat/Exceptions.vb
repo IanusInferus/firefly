c2eae94f1e79b4f156a3e1a4ec62300cad2f0a88
Ianus Inferus <r_ex@163.com>
2013-03-29 02:32:56 +0000

    Firefly.Core.Texting.TreeFormat: 修正异常处理的一处错误。 修正换行出错时的错误信息没有包含行号的问题。

--- Src/FireflyCore/Texting/TreeFormat/Exceptions.vb
+++ Src/FireflyCore/Texting/TreeFormat/Exceptions.vb
@@ -3,7 +3,7 @@
 '  File:        Exceptions.vb
 '  Location:    Firefly.Texting.TreeFormat <Visual Basic .Net>
 '  Description: 异常定义
-'  Version:     2012.07.23.
+'  Version:     2013.03.28.
 '  Copyright(C) F.R.C.
 '
 '==========================================================================
@@ -48,7 +48,7 @@ Namespace Texting.TreeFormat.Syntax
 
         Protected Shared Function GetMessage(ByVal Message As String, ByVal Range As FileTextRange) As String
             Dim l As New List(Of String)
-            If Range.Text.Path <> "" Then l.Add(Range.Text.Path)
+            If Range.Text IsNot Nothing AndAlso Range.Text.Path <> "" Then l.Add(Range.Text.Path)
             If Range.Range.HasValue Then
                 l.Add(Range.Range.Value.ToString())
             End If
