7795cd04dd0ebaa5e52d6cfc2204e66f8f8a246b
Ianus Inferus <r_ex@163.com>
2018-09-09 09:52:28 +0000

    Firefly.Lite: 增加.Net Standard 2.0支持，仍然支持.Net Framework 4.0。 增加对VS2017编译环境的要求。 Firefly.Core: 修正一些代码分析错误。

--- Src/FireflyCore/AssemblyInfo.vb
+++ Src/FireflyCore/AssemblyInfo.vb
@@ -30,4 +30,4 @@ Imports System.Runtime.InteropServices
 ' 您可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，方法是按
 ' 如下所示使用 '*':
 
-<Assembly: System.Reflection.AssemblyVersion("2017.05.21.0")>
+<Assembly: System.Reflection.AssemblyVersion("2018.09.09.0")>
