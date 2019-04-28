b71be6fd593c2fc509c1f2769ec112894eae191f
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2019-04-28 12:35:22 +0800

    *: 整理工程和构建脚本。

--- Src/Build.cmd
+++ Src/Build.cmd
@@ -1,17 +1,29 @@
 @echo off
 
-for %%p in (Enterprise Professional Community BuildTools) do (
-  if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\%%p" (
-    set VSDir="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\%%p"
+setlocal
+if "%SUB_NO_PAUSE_SYMBOL%"=="1" set NO_PAUSE_SYMBOL=1
+if /I "%COMSPEC%" == %CMDCMDLINE% set NO_PAUSE_SYMBOL=1
+set SUB_NO_PAUSE_SYMBOL=1
+call :main %*
+set EXIT_CODE=%ERRORLEVEL%
+if not "%NO_PAUSE_SYMBOL%"=="1" pause
+exit /b %EXIT_CODE%
+
+:main
+for %%v in (2019 2017) do (
+  for %%p in (Enterprise Professional Community BuildTools) do (
+    for %%b in (Current 15.0) do (
+      if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\%%v\%%p\MSBuild\%%b\Bin\MSBuild.exe" (
+        set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\%%v\%%p\MSBuild\%%b\Bin\MSBuild.exe"
+        goto MSBuild_Found
+      )
+    )
   )
 )
-set VSDir=%VSDir:"=%
-echo VSDir=%VSDir%
+:MSBuild_Found
 
-"%VSDir%\MSBuild\15.0\Bin\MSBuild.exe" Firefly.sln /restore /t:Rebuild /p:Configuration=Release
+%MSBuild% Firefly.sln /restore /t:Rebuild /p:Configuration=Release || exit /b 1
 
-copy Doc\Readme.*.txt ..\Bin\
-copy Doc\UpdateLog.*.txt ..\Bin\
-copy Doc\License.*.txt ..\Bin\
-
-pause
+copy Doc\Readme.*.txt ..\Bin\ || exit /b 1
+copy Doc\UpdateLog.*.txt ..\Bin\ || exit /b 1
+copy Doc\License.*.txt ..\Bin\ || exit /b 1
