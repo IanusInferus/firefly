19518958c89bccf3099de3e3f78d64777cd46dda
Ianus Inferus <10500058+IanusInferus@users.noreply.github.com>
2021-12-17 22:18:10 +0800

    *: 去除文档生成。

--- Src/BuildDoc.cmd
+++
@@ -1,5 +0,0 @@
-PATH %SystemRoot%\Microsoft.NET\Framework\v4.0.30319;%PATH%
-
-MSBuild FireflyDoc.shfbproj /t:Rebuild /p:Configuration=Release
-
-pause
--- Src/Publish.cmd
+++ Src/Publish.cmd
@@ -10,4 +10,4 @@
 @call Clear.cmd
 @cd ..
 @if not exist ..\Versions\ md ..\Versions\
-@rar a -av- -m5 -md4096 -tsm -tsc -s -k -t ..\Versions\%PackName%.rar -x*\.*\ -x*.user -x*.suo Src Bin Manual Examples
+@rar a -av- -m5 -md4096 -tsm -tsc -s -k -t ..\Versions\%PackName%.rar -x*\.*\ -x*.user -x*.suo Src Bin Examples
